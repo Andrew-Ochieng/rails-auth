c8a54149516bf37ba060e529be9704aec7ecaee7
Code-X <andrewomosh44@gmail.com>
2023-03-24 18:07:34 +0300

    create new users seed-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,18 @@
 require 'faker'
 
 puts "Seeding Users"
-# Users.destroy_all
+User.destroy_all
 
 users = []
-passwords = [
-    {id: 1, name: "@12345"}, 
-    {id: 2, name: "@54321"}, 
-    {id: 3, name: "@12354"}, 
-    {id: 4, name: "@54123"},
-    {id: 5, name: "@15243"}
-]
 
 5.times do
-    users << User.create(
-        username: Faker::Internet.username,
-        email: Faker::Internet.email,
-        password: passwords.sample.id
-    )
+    users << User.create([
+        {username: "Andrew", email: "andrew@gmail.com", password: "Andrew@_2023"},
+        {username: "Jones", email: "jones@gmail.com", password: "Jones@_2023"},
+        {username: "Maureen", email: "maureen@gmail.com", password: "Maureen@_2023"},
+        {username: "Cathy", email: "cathy@gmail.com", password: "Cathy@_2023"},
+        {username: "Daniel", email: "daniel@gmail.com", password: "Daniel@_2023"}
+    ])
 end
 
 
